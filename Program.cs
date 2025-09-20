a16d3b46983e82465c90d48938da5ac7c5121afd
Tomas178 <tomaspetronis77@gmail.com>
2025-09-20 21:33:44 +0300

    Add every repository to program services

--- Program.cs
+++ Program.cs
@@ -1,8 +1,18 @@
 using Project.Database;
+using Project.Repositories;
+using Project.Repositories.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration["ConnectionStrings:DefaultConnection"];
+
+builder.Services.AddScoped<IUsersRepository, UsersRepository>();
+builder.Services.AddScoped<IRecipesRepository, RecipesRepository>();
+builder.Services.AddScoped<ISavedRecipesRepository, SavedRecipesRepository>();
+builder.Services.AddScoped<IToolsRepository, ToolsRepository>();
+builder.Services.AddScoped<IRecipesToolsRepository, RecipesToolsRepository>();
+builder.Services.AddScoped<IIngredientsRepository, IngredientsRepository>();
+builder.Services.AddScoped<IRecipesIngredientsRepository, RecipesIngredientsRepository>();
 
 builder.Services.AddDbContext<ProjectDbContext>(options =>
     options.UseNpgsql(connectionString));
