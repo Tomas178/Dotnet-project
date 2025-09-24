0ac1ca701e4e1a00abe933e724e8ab4de05823c5
Tomas178 <tomaspetronis77@gmail.com>
2025-09-24 21:42:50 +0300

    Fix Recipes DTO

--- Models/Dtos/Recipes/UpdateRecipesRequestDto.cs
+++ Models/Dtos/Recipes/UpdateRecipesRequestDto.cs
@@ -3,6 +3,7 @@ namespace Project.Models.Dtos.Recipes;
 public class UpdateRecipesRequestDto
 {
     public int Id { get; set; }
+    public int UserId { get; set; }
     public string Title { get; set; } = string.Empty;
     public string Steps { get; set; } = string.Empty;
     public int Duration { get; set; }
