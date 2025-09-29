0a511d0fecbd8b0c5c10aa6a95fb906399570eca
Tomas178 <tomaspetronis77@gmail.com>
2025-09-29 19:12:30 +0300

    dont return userId

--- Models/Dtos/Recipes/RecipesResponseDto.cs
+++ Models/Dtos/Recipes/RecipesResponseDto.cs
@@ -5,7 +5,6 @@ using Project.Models.Dtos.Users;
 public class RecipesResponseDto
 {
     public int Id { get; set; }
-    public int UserId { get; set; }
     public UsersResponseDto User { get; set; } = null!;
     public string Title { get; set; } = string.Empty;
     public List<string> Steps { get; set; } = [];
